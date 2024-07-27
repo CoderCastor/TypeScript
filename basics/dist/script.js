f1a8e0a64c9c16c77f0a085930d65cf9a0a52781
CoderCastor <tejasshinde935@gmail.com>
2024-07-27 08:42:55 +0530

    config changes

--- basics/dist/script.js
+++ basics/dist/script.js
@@ -1,51 +1,37 @@
+"use strict";
 // types
 //primitive and reference
-
 // let arr = [1,2,3,4,5];
 // let arr2 = arr;
 // arr2.pop;
-
 // let a;  //don't do this
-
 // let a : number;
 // let array : [];
 // let string : string = "Hello";
-
 // // if you are giving value to the variable then do not declaire there datatype in that case
-
 // // tuples
 // let tpl : [number,string] = [1,"hi"];
-
-
-let b : any;  //here the type is any initial
+let b; //here the type is any initial
 b = "tejas";
-b =12;
-
+b = 12;
 //like that there is "unknown" type same as any
-let naming : unknown;
+let naming;
 naming = "rakesh";
 naming = true;
-
 //there are some other types --> never,void
-
 //here we need to specify what the fn is returning if it gets change then throw error
-function abcd() : number {
+function abcd() {
     return 12;
 }
 //it will help u to use methods for the returning datatype while calling the functions like "abcd()."
-
-
 // error Code
 // function erer(): string{
 //     return 12;
 // }
-
 //likewise we have to return void if function in not returning any thing for example -->
 // function vaaid() : void {
 //     let a = "hello";
 // }
-
-
 // enum -- help to design object like datatypes
 // eg -->
 // enum Human{
@@ -54,43 +40,18 @@ function abcd() : number {
 //     roll = 323
 // }
 // console.log(Human.name); --->"tejas"
-
-
-
 // type Inference - just guessing the datatype by its own
 //now we can use || or for declaring variable datatypes like -->> let a : number | string;
-
-function fn(variable : number | string){
-    if(typeof variable ==='number'){
+function fn(variable) {
+    if (typeof variable === 'number') {
         variable.toFixed(2);
     }
-    else if(typeof variable === 'string'){
+    else if (typeof variable === 'string') {
         variable.toUpperCase();
     }
 }
-
-// fn(12);          it works
-// fn("12");        it works
-// fn("hero");      it works
-// fn(true);        not works
-
-// Intersection 
-// we can't take like ---> let a : number & string;
-
-type City = {
-    name : string,
-    population : number
-}
-
-type Capital = {
-    primary : boolean,
-    name : string
-}
-
-type CapitalinCities = City & Capital;
-
-let value : CapitalinCities = {
-    name : "Pune",
-    population : 1212121212,
-    primary : true
-}
+let value = {
+    name: "Pune",
+    population: 1212121212,
+    primary: true
+};
